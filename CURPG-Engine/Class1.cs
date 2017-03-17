c6d290a3d5c3438a297a77f59caab50bc344a4bf
Hamish <trust-no-1@adam.com.au>
2017-03-17 10:46:08 +1030

    Add project files.

---
+++ CURPG-Engine/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURPG_Engine
+{
+    public class Class1
+    {
+    }
+}
