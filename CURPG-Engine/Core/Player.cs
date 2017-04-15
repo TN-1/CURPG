TN-1/CURPG

commit 5e398fc5f744e1ae495533d408c5291a8ee35b09

Implement a screen manager

CURPG_Graphical_MonoGame_Windows/ScreenManager.cs
@@ -1,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using CURPG_Graphical_MonoGame_Windows.Screens;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using QuakeConsole;
+
+namespace CURPG_Graphical_MonoGame_Windows
+{
+    public class ScreenManager : Game
+    {
+        public static GraphicsDeviceManager GraphicsDeviceMgr;
+        public static SpriteBatch Sprites;
+        public static Dictionary<string, Texture2D> Textures2D;
+        public static Dictionary<string, SpriteFont> Fonts;
+        public static List<GameScreen> ScreenList;
+        public static ContentManager ContentMgr;
+        public static ConsoleComponent console;
+        public static PythonInterpreter interpreter;
+        public static System.Drawing.Rectangle ScreenArea;
+
+
+        public ScreenManager()
+        {
+            var pt = new System.Drawing.Point(0, 0);
+            ScreenArea = System.Windows.Forms.Screen.GetWorkingArea(pt);
+            GraphicsDeviceMgr = new GraphicsDeviceManager(this);
+            GraphicsDeviceMgr.IsFullScreen = true;
+            GraphicsDeviceMgr.PreferredBackBufferHeight = ScreenArea.Height;
+            GraphicsDeviceMgr.PreferredBackBufferWidth = ScreenArea.Width;
+            GraphicsDeviceMgr.IsFullScreen = false;
+            Window.Title = "CURPG";
+            Window.AllowUserResizing = false;
+
+            Content.RootDirectory = "Content";
+
+            //Setup console
+            console = new ConsoleComponent(this);
+            Components.Add(console);
+            console.FontColor = Color.Aqua;
+            console.InputPrefixColor = Color.Aqua;
+            console.InputPrefix = ">";
+            interpreter = new PythonInterpreter();
+            console.Interpreter = interpreter;
+
+        }
+
+        protected override void Initialize()
+        {
+            Textures2D = new Dictionary<string, Texture2D>();
+            Fonts = new Dictionary<string, SpriteFont>();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            ContentMgr = Content;
+            Sprites = new SpriteBatch(GraphicsDevice);
+
+            // Load any full game assets here
+
+            AddScreen(new PlayScreen());
+        }
+
+        protected override void UnloadContent()
+        {
+            foreach (var screen in ScreenList)
+            {
+                screen.UnloadAssets();
+            }
+            Textures2D.Clear();
+            Fonts.Clear();
+            ScreenList.Clear();
+            Content.Unload();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            try
+            {
+                // TODO Remove temp code
+                if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                {
+                    Exit();
+                }
+
+                var startIndex = ScreenList.Count - 1;
+                while (ScreenList[startIndex].IsPopup && ScreenList[startIndex].IsActive)
+                {
+                    startIndex--;
+                }
+                for (var i = startIndex; i < ScreenList.Count; i++)
+                {
+                    ScreenList[i].Update(gameTime);
+                }
+            }
+            catch (Exception ex)
+            {
+                // ErrorLog.AddError(ex);
+                throw ex;
+            }
+            finally
+            {
+                base.Update(gameTime);
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            var startIndex = ScreenList.Count - 1;
+            while (ScreenList[startIndex].IsPopup)
+            {
+                startIndex--;
+            }
+
+            GraphicsDevice.Clear(ScreenList[startIndex].BackgroundColor);
+            GraphicsDeviceMgr.GraphicsDevice.Clear(ScreenList[startIndex].BackgroundColor);
+
+            for (var i = startIndex; i < ScreenList.Count; i++)
+            {
+                ScreenList[i].Draw(gameTime);
+            }
+
+            base.Draw(gameTime);
+        }
+
+        public static void AddFont(string fontName)
+        {
+            if (Fonts == null)
+            {
+                Fonts = new Dictionary<string, SpriteFont>();
+            }
+            if (!Fonts.ContainsKey(fontName))
+            {
+                Fonts.Add(fontName, ContentMgr.Load<SpriteFont>(fontName));
+            }
+        }
+
+        public static void RemoveFont(string fontName)
+        {
+            if (Fonts.ContainsKey(fontName))
+            {
+                Fonts.Remove(fontName);
+            }
+        }
+
+        public static void AddTexture2D(string textureName)
+        {
+            if (Textures2D == null)
+            {
+                Textures2D = new Dictionary<string, Texture2D>();
+            }
+            if (!Textures2D.ContainsKey(textureName))
+            {
+                Textures2D.Add(textureName, ContentMgr.Load<Texture2D>(textureName));
+            }
+        }
+
+        public static void RemoveTexture2D(string textureName)
+        {
+            if (Textures2D.ContainsKey(textureName))
+            {
+                Textures2D.Remove(textureName);
+            }
+        }
+
+        public static void AddScreen(GameScreen gameScreen)
+        {
+            if (ScreenList == null)
+            {
+                ScreenList = new List<GameScreen>();
+            }
+            ScreenList.Add(gameScreen);
+            gameScreen.Initialize();
+            gameScreen.LoadAssets();
+        }
+
+        public static void RemoveScreen(GameScreen gameScreen)
+        {
+            gameScreen.UnloadAssets();
+            ScreenList.Remove(gameScreen);
+            if (ScreenList.Count < 1)
+                AddScreen(new GameScreen()); // Default screen
+        }
+
+        public static void ChangeScreens(GameScreen currentScreen, GameScreen targetScreen)
+        {
+            RemoveScreen(currentScreen);
+            AddScreen(targetScreen);
+        }
+
+        /// <summary>
+        /// Hooks into the OnExiting event to save the game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        protected override void OnExiting(Object sender, EventArgs args)
+        {
+            base.OnExiting(sender, args);
+
+            foreach(GameScreen screen in ScreenList)
+            {
+                if(screen is PlayScreen play)
+                {
+                    var status = CURPG_Engine.Core.Persistance.SaveGame(play.world, play.player);
+                    if (status == 0)
+                    {
+                        System.Windows.Forms.MessageBox.Show("Save Failed. Do you want to close?", "Error", System.Windows.Forms.MessageBoxButtons.RetryCancel);
+                    }
+                }
+            }
+        }
+    }
+}

CURPG_Graphical_MonoGame_Windows/Screens/GameScreen.cs
@@ -1,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CURPG_Graphical_MonoGame_Windows.Screens
+{
+    public class GameScreen
+    {
+        public bool IsActive = true;
+        public bool IsPopup = false;
+        public Color BackgroundColor = Color.White;
+
+        public virtual void Initialize() { }
+        public virtual void LoadAssets() { }
+        public virtual void Update(GameTime gameTime) { }
+        public virtual void Draw(GameTime gameTime) { }
+        public virtual void UnloadAssets() { }
+    }
+}

commit 45f69663ea3d8fdce4900f0d50159f7baa30f876

Draws inventory, Allows selectable items, Fixes serialisation(Broken in 5e398fc)

CURPG-Engine/Core/Player.cs
@@ -82,9 +82,8 @@ namespace CURPG_Engine.Core
                             LocationY = newY;
                             break;
                         case 1:
-                            foreach (var item in Inventory.Items)
+                            if (Inventory.Items[0] is Tool tool && tool.TerrainMod == 1)
                             {
-                                if (!(item is Tool tool) || tool.TerrainMod != 1) continue;
                                 LocationX = newX;
                                 LocationY = newY;
                                 world.ChangeTile(LocationX, LocationY, 24);
@@ -92,21 +91,24 @@ namespace CURPG_Engine.Core
                                 foreach (var check in Inventory.ItemDb)
                                     if (check.Id == 1)
                                         logs = (Craftable) check;
-                                if (logs == null) continue;
-                                logs.StackHeight = r.Next(1, 5);
-                                if (Inventory.Contains(1))
+                                if (logs != null)
                                 {
-                                    foreach (var log in Inventory.Items)
-                                        if (log.Id == 1)
-                                        {
-                                            var exists = (Craftable) log;
-                                            exists.AddQuantity(logs.StackHeight);
-                                        }
+                                    logs.StackHeight = r.Next(1, 5);
+                                    if (Inventory.Contains(1))
+                                    {
+                                        foreach (var log in Inventory.Items)
+                                            if (log.Id == 1)
+                                            {
+                                                var exists = (Craftable) log;
+                                                exists.AddQuantity(logs.StackHeight);
+                                            }
+                                    }
+                                    else
+                                        Inventory.AddItem(logs);
                                 }
-                                else
-                                    Inventory.AddItem(logs);
+                                return;
                             }
-                            return;
+                            return; ;
                         case 2:
                             //Mountains
                             return;

CURPG-Engine/Inventory/Manager.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Xml;
 // ReSharper disable UnusedMember.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Local
 
 namespace CURPG_Engine.Inventory
 {
@@ -14,29 +17,37 @@ namespace CURPG_Engine.Inventory
     [Serializable]
     public class Inventory
     {
-        public readonly Item[] Items;
+        public Item[] Items;
         public List<Item> ItemDb;
-        private readonly int _capacity;
 
         public Inventory(int capacity)
         {
-            _capacity = capacity;
-            Items = new Item[_capacity];
+            Items = new Item[capacity];
         }
+
+        private void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
 
         /// <summary>
         /// Finds the index of the first available slot
         /// </summary>
         /// <returns>Index of first avail slot</returns>
-        int FirstAvailSlot()
+        private int FirstAvailSlot(Item[] array)
         {
-            if (Items != null)
+            for (var i = 0; i < array.Length; i++)
             {
-                for (int i = 0; i < _capacity; i++)
-                {
-                    if (Items[i] == null)
-                        return i;
-                }
+                if (array[i] == null)
+                    return i;
             }
             return -1;
         }
@@ -47,8 +58,9 @@ namespace CURPG_Engine.Inventory
         public void Clear()
         {
             if (Items == null) return;
-            for (var i = 0; i < _capacity; i++)
+            for (var i = 0; i < Items.Length; i++)
                 Items[i] = null;
+            OnPropertyChanged("Items");
         }
 
         /// <summary>
@@ -57,7 +69,8 @@ namespace CURPG_Engine.Inventory
         /// <param name="item">Item class of item to add</param>
         public void AddItem(Item item)
         {
-            Items[FirstAvailSlot()] = item;
+            Items[FirstAvailSlot(Items)] = item;
+            OnPropertyChanged("Items");
         }
 
         /// <summary>
@@ -66,9 +79,13 @@ namespace CURPG_Engine.Inventory
         /// <param name="i">Item ID to add to inventory</param>
         public void AddItem(int i)
         {
-            foreach(var item in ItemDb)
-                if(item.Id == i)
-                    Items[FirstAvailSlot()] = item;
+            var j = FirstAvailSlot(Items);
+            foreach (var item in ItemDb)
+            {
+                if (item.Id == i)
+                    Items[j] = item;
+            }
+            OnPropertyChanged("Items");
         }
 
         /// <summary>
@@ -176,5 +193,20 @@ namespace CURPG_Engine.Inventory
             return Items.Select(item => item.Id == id).FirstOrDefault();
         }
 
+        public void MakeActive(int index)
+        {
+            Item iitem = Items[0];
+            Item item = Items[index];
+            Items[index] = iitem;
+            Item[] temp = new Item[Items.Length];
+            temp[0] = item;
+            for (var i = 1; i <= (Items.Length - 1); i++)
+            {
+                if (Items[i] == null) continue;
+                temp[FirstAvailSlot(temp)] = Items[i];
+            }
+            Items = temp;
+            OnPropertyChanged("Items");
+        }
     }
 }

CURPG_Graphical_MonoGame_Windows/ScreenManager.cs
@@ -14,15 +14,15 @@ namespace CURPG_Graphical_MonoGame_Windows
     {
         public static GraphicsDeviceManager GraphicsDeviceMgr;
         public static SpriteBatch Sprites;
-        private static Dictionary<string, Texture2D> _textures2D;
-        private static Dictionary<string, SpriteFont> _fonts;
-        private static List<GameScreen> _screenList;
         public static ContentManager ContentMgr;
         public static ConsoleComponent Console;
         public static PythonInterpreter Interpreter;
         public static System.Drawing.Rectangle ScreenArea;
         private static Dictionary<string, GameScreen> _screens;
-
+        [NonSerialized] private static Dictionary<string, Texture2D> _textures2D;
+        [NonSerialized] private static Dictionary<string, SpriteFont> _fonts;
+        [NonSerialized] private static List<GameScreen> _screenList;
+        [NonSerialized] private KeyboardState _oldState;
 
         public ScreenManager()
         {
@@ -86,6 +86,8 @@ namespace CURPG_Graphical_MonoGame_Windows
 
         protected override void Update(GameTime gameTime)
         {
+            var newState = Keyboard.GetState();  // get the newest state
+
             try
             {
                 // TODO Remove temp code
@@ -93,6 +95,8 @@ namespace CURPG_Graphical_MonoGame_Windows
                 {
                     Exit();
                 }
+                if (_oldState.IsKeyUp(Keys.OemTilde) && newState.IsKeyDown(Keys.OemTilde))
+                    Console.ToggleOpenClose();
 
                 var startIndex = _screenList.Count - 1;
                 while (GameScreen.IsPopup && GameScreen.IsActive)
@@ -108,6 +112,7 @@ namespace CURPG_Graphical_MonoGame_Windows
             {
                 base.Update(gameTime);
             }
+            _oldState = newState;
         }
 
         protected override void Draw(GameTime gameTime)

CURPG_Graphical_MonoGame_Windows/Screens/GameScreen.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 // ReSharper disable ConvertToConstant.Global
 // ReSharper disable FieldCanBeMadeReadOnly.Global
 
 namespace CURPG_Graphical_MonoGame_Windows.Screens
 {
+    [Serializable]
     public class GameScreen
     {
-        public static bool IsActive = true;
-        public static bool IsPopup = false;
-        public Color BackgroundColor = Color.White;
+        [NonSerialized] public static bool IsActive = true;
+        [NonSerialized] public static bool IsPopup = false;
+        [NonSerialized] public Color BackgroundColor = Color.White;
 
         public virtual void Initialize() { }
         public virtual void LoadAssets() { }

CURPG_Graphical_MonoGame_Windows/Screens/MenuScreen.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace CURPG_Graphical_MonoGame_Windows.Screens
 {
-    class MenuScreen : GameScreen
+    internal class MenuScreen : GameScreen
     {
         private KeyboardState _oldState;
 
@@ -16,9 +16,6 @@ namespace CURPG_Graphical_MonoGame_Windows.Screens
         public override void Update(GameTime gameTime)
         {
             var newState = Keyboard.GetState();  // get the newest state
-
-            if (_oldState.IsKeyUp(Keys.OemTilde) && newState.IsKeyDown(Keys.OemTilde))
-                ScreenManager.Console.ToggleOpenClose();
 
             _oldState = newState;
 

CURPG_Graphical_MonoGame_Windows/Screens/PlayScreen.cs
@@ -12,18 +12,23 @@ using NLua;
 
 namespace CURPG_Graphical_MonoGame_Windows.Screens
 {
+    [Serializable]
     public partial class PlayScreen : GameScreen
     {
-        readonly Dictionary<string, Texture2D> _tileTextures = new Dictionary<string, Texture2D>();
+        [NonSerialized] private readonly Dictionary<string, Texture2D> _tileTextures = new Dictionary<string, Texture2D>();
         public World World;
         private List<Tile> _tileSet;
         public Player Player;
-        private KeyboardState _oldState;
-        System.Drawing.Rectangle _mapArea;
-        private Camera _camera;
-        private Panel _bottomPanel;
-        private Panel _rightPanel;
-        private readonly Lua _lua = new Lua();
+        [NonSerialized] private KeyboardState _oldState;
+        [NonSerialized] private System.Drawing.Rectangle _mapArea;
+        [NonSerialized] private Camera _camera;
+        [NonSerialized] private Panel _bottomPanel;
+        [NonSerialized] private Panel _rightPanel;
+        [NonSerialized] private PanelTabs _tabs;
+        [NonSerialized] private PanelTabs.TabData _invTab;
+        [NonSerialized] private PanelTabs.TabData _statTab;
+
+        [NonSerialized] private readonly Lua _lua = new Lua();
 
         public override void Initialize()
         {
@@ -35,13 +40,17 @@ namespace CURPG_Graphical_MonoGame_Windows.Screens
                 World = Persistance.LoadWorld();
                 Player = Persistance.LoadPlayer();
                 _tileSet = World.TileSet;
+                var loc = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                if (loc == null) throw new Exception("Loc is null");
+                var itemsPath = Path.Combine(loc, @"DataFiles\Items.xml");
+                Player.Inventory.BuildDatabase(itemsPath);
 
                 if (World == null || Player == null)
                 {
-                    var loc = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                    loc = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
                     if(loc == null) throw new Exception("Loc is null");
                     var tilesPath = Path.Combine(loc, @"DataFiles\Tiles.xml");
-                    var itemsPath = Path.Combine(loc, @"DataFiles\Items.xml");
+                    itemsPath = Path.Combine(loc, @"DataFiles\Items.xml");
                     _tileSet = WorldTools.TileSetBuilder(tilesPath);
                     World = WorldTools.GenerateWorld(0, 128, 128, _tileSet, "World", 24);
                     var pt = PlayerTools.GetSpawn(World, _mapArea.Width / 2, _mapArea.Height / 2);
@@ -81,7 +90,7 @@ namespace CURPG_Graphical_MonoGame_Windows.Screens
 
         public override void LoadAssets()
         {
-            foreach (Tile tile in _tileSet)
+            foreach (var tile in _tileSet)
             {
                 try
                 {
@@ -107,6 +116,8 @@ namespace CURPG_Graphical_MonoGame_Windows.Screens
         {
             var newState = Keyboard.GetState();  // get the newest state
 
+            Player.Inventory.PropertyChanged += Inventory_PropertyChanged;
+
             // handle the input
             if (_oldState.IsKeyUp(Keys.Left) && newState.IsKeyDown(Keys.Left))
             {
@@ -127,15 +138,16 @@ namespace CURPG_Graphical_MonoGame_Windows.Screens
                 Player.MovePlayer(0, 1, World);
             }
 
-            if (_oldState.IsKeyUp(Keys.OemTilde) && newState.IsKeyDown(Keys.OemTilde))
-                ScreenManager.Console.ToggleOpenClose();
-
-
             _oldState = newState;  // set the new state as the old state for next time
 
             UserInterface.Update(gameTime);
 
             base.Update(gameTime);
+        }
+
+        private void Inventory_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            DrawInv();
         }
 
         public override void Draw(GameTime gameTime)

CURPG_Graphical_MonoGame_Windows/Screens/PlayScreen_UI.cs
@@ -2,35 +2,105 @@
 using GeonBit.UI.Entities;
 using Microsoft.Xna.Framework;
 using System;
+using System.Collections.Generic;
+using GeonBit.UI.DataTypes;
+using System.Diagnostics;
 
 namespace CURPG_Graphical_MonoGame_Windows.Screens
 {
     partial class PlayScreen
     {
+
         private void Ui()
         {
             UserInterface.Initialize(ScreenManager.ContentMgr, BuiltinThemes.hd);
 
             //Right Panel
-            _rightPanel = new Panel(new Vector2(Convert.ToInt32((ScreenManager.ScreenArea.Width * .5) - 16), Convert.ToInt32(ScreenManager.ScreenArea.Height - 70)), PanelSkin.Default, Anchor.TopRight, offset: new Vector2(0, 70));
-            var tabs = new PanelTabs();
-            var invTab = tabs.AddTab("Inventory");
-            var statTab = tabs.AddTab("Skills");
+            _rightPanel = new Panel(new Vector2(Convert.ToInt32((ScreenManager.ScreenArea.Width * .5) - 16), Convert.ToInt32(ScreenManager.ScreenArea.Height - 70)), PanelSkin.Default, Anchor.TopRight, new Vector2(0, 70));
+            _tabs = new PanelTabs();
+            _invTab = _tabs.AddTab("Inventory");
+            _statTab = _tabs.AddTab("Skills");
 
 
-            invTab.panel.AddChild(new Header("Hello inventory!"));
-            var icon = new Icon(IconType.Sword, anchor: Anchor.TopLeft, scale: 1.2f, background: true, offset: new Vector2(10, 10));
-            invTab.panel.AddChild(icon);
+            _invTab.panel.AddChild(new Header("Hello inventory!"));
+            _statTab.panel.AddChild(new Header("Hello stats!"));
 
-            statTab.panel.AddChild(new Header("Hello stats!"));
+            var stackTrace = new StackTrace();
+            if(stackTrace.GetFrame(1).GetMethod().Name != "DrawInv")
+                DrawInv();
 
-
-            _rightPanel.AddChild(tabs);
+            _rightPanel.AddChild(_tabs);
             UserInterface.AddEntity(_rightPanel);
 
             //Bottom Panel
             _bottomPanel = new Panel(new Vector2(Convert.ToInt32((ScreenManager.ScreenArea.Width * .5) + (16 - Convert.ToInt32((ScreenManager.ScreenArea.Height * .3) - 16))), Convert.ToInt32((ScreenManager.ScreenArea.Height * .3) - 16)), PanelSkin.Default, Anchor.BottomRight, new Vector2(Convert.ToInt32((ScreenManager.ScreenArea.Width * .5) - 16), 0));
             UserInterface.AddEntity(_bottomPanel);
+
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
+        public void DrawInv()
+        {
+            //BUG: Optimise this!
+            if (_rightPanel == null)
+                Ui();
+
+            var width = _rightPanel.Size.X;
+            var rowSize = (float)Math.Floor(width / 65);
+            _invTab.panel.ClearChildren();
+
+            var icons = new Dictionary<string, IconType> {{"axe", IconType.Axe}, {"log", IconType.Apple}};
+            for (var i = 0; i < Player.Inventory.Items.Length; i++)
+            {
+                var item = Player.Inventory.Items[i];
+                if (item == null) continue;
+                var icon = new IconI(i, icons[item.EntityName], Anchor.TopLeft, 1.2f, true, new Vector2(10, 10))
+                {
+                    OnClick = (Entity icn) =>
+                    {
+                        if(icn is IconI icnI)
+                        Player.Inventory.MakeActive(icnI.Index);
+                    },
+                    WhileMouseHover = (Entity icn) =>
+                    {
+                        //TODO: Handle mouse hover events
+                    }
+                };
+                //TODO: Finish off this.
+                if(i == 0)
+                    icon.SetPosition(Anchor.TopCenter, new Vector2(0));
+                else if (i - 1 < rowSize)
+                    icon.SetPosition(Anchor.TopLeft, new Vector2(65 * (i - 1), 65));
+                else if (i - (1 + rowSize) < rowSize)
+                    icon.SetPosition(Anchor.TopLeft, new Vector2(65 * (i - (1 + rowSize)), 65 * 2));
+
+                _invTab.panel.AddChild(icon);
+            }
+        }
+    }
+
+    class IconI : Icon
+    {
+        public int Index;
+
+        public IconI(int index, IconType icon, Anchor anchor = Anchor.Auto, float scale = 1, bool background = false, Vector2? offset = default(Vector2?)) : base(icon, anchor, scale, background, offset)
+        {
+            Index = index;
+            Scale = scale;
+            DrawBackground = background;
+            Texture = Resources.IconTextures[(int)icon];
+
+            // set default background color
+            SetStyleProperty("BackgroundColor", new StyleProperty(Color.White));
+
+            // if have background, add default space-after
+            if (background)
+            {
+                SpaceAfter = Vector2.One * BackgroundSize;
+            }
+
+            // update default style
+            UpdateStyle(DefaultStyle);
 
         }
     }